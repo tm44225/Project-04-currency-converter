855977ef510dc5a8254292a5ca394d96dcea0ff9
tm44225 <127407748+tm44225@users.noreply.github.com>
2024-07-01 15:15:11 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+
+let exchange_rate: any = {
+    "USD": 1,
+    "EUR": 0.9,
+    "JYP": 113,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 277.70,
+}
+
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "select the currency to convert from",
+        choices: ["USD","EUR","JYP","CAD","AUD","PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "select the currency to convert into:",
+        choices: ["USD","EUR","JYP","CAD","AUD","PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter the amount to convert:"
+    }
+]);
+
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount
+
+// formula for converstion
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+
+console.log(`converted amount = ${converted_amount.toFixed(2)}`);
